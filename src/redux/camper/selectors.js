b3019a72c611d88f4929f5b0d2e9f0767a733974
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-07-21 16:59:28 +0300

    fixed selectors

--- src/redux/camper/selectors.js
+++ src/redux/camper/selectors.js
@@ -1,4 +1,4 @@
-export const selectCampers = (state) => state.campers.campers;
-export const selectFavorites = (state) => state.campers.favorites;
-export const selectIsLoading = (state) => state.campers.isLoading;
-export const selectError = (state) => state.campers.error;
+export const selectCampers = (state) => state.camper.campers;
+export const selectFavorites = (state) => state.camper.favorites;
+export const selectIsLoading = (state) => state.camper.isLoading;
+export const selectError = (state) => state.camper.error;
