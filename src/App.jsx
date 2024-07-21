2695d9a17cb2d535c9270d445b8818c62d27eceb
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-07-21 16:32:51 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
+import Layout from './components/Layout';
+
+const HomePage = lazy(() => import('./pages/Home/Home'));
+const CatalogPage = lazy(() => import('./pages/Catalog/Catalog'));
+const FavoritesPage = lazy(() => import('./pages/Favorites/Favorites'));
+
 const App = () => {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <Layout>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Layout>
   );
 };
 
