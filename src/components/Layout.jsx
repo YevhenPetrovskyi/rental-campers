cb5f263ed2181273789ab2b47b0d2faa7196f446
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-07-21 14:43:59 +0300

    add Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
 import { Suspense } from 'react';
+
 import Container from './Container/Container';
 import Loader from './Loader/Loader';
+import Menu from './Menu/Menu';
 
 const Layout = ({ children }) => {
   return (
-    <Container>
-      <Suspense fallback={<Loader />}>{children}</Suspense>
-    </Container>
+    <>
+      <Menu />
+
+      <Container>
+        <Suspense fallback={<Loader />}>{children}</Suspense>
+      </Container>
+    </>
   );
 };
 
