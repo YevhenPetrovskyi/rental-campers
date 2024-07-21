1789df472bfe50345ed7ac9d71fdcc244b499393
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-07-21 20:50:03 +0300

    Home page ready

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,30 @@
+import { NavLink } from 'react-router-dom';
+
+import DocumentTitle from '../../components/DocumentTitle';
+
+import css from './Home.module.css';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <DocumentTitle>Home - Rental Campers</DocumentTitle>
+
+      <section>
+        <div className={css.homeWrapper}>
+          <h1 className={css.homeTitle}>Welcome to Rental Campers</h1>
+          <p className={css.subtitle}>
+            Rent an RV from a local owner in Ukraine
+          </p>
+          <p className={css.text}>
+            Book your perfect motorhome easily, safely and affordably
+          </p>
+          <NavLink to="/catalog" className={css.link}>
+            Rent now
+          </NavLink>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
